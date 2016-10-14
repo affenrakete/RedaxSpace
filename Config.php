9c5d9de8e09af98d38de099154e68dc19ddfe0df
nkler <ps-github@nkio.de>
2016-10-14 13:47:49 +0200

    Adding new files

---
+++ Config.php
@@ -0,0 +1,29 @@
+<?php
+namespace Redaxscript\Modules\RedaxSpace;
+
+use Redaxscript\Modules;
+
+/**
+ * Use Redaxscript to controll parts of Uberspace
+ *
+ * @since 3.0.0
+ *
+ * @package Redaxscript
+ * @category Modules
+ * @author Peter Siemer
+ */
+
+class Config extends Module
+{
+    /**
+     * array of config
+     *
+     * @var array
+     */
+    protected static $_configArray =
+        [
+
+        ];
+}
+
+?>
